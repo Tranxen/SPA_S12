b787323bf7610828c3fa9e1a75a42f8cb075b5d4
tranxen <fabien.berarde@gmail.com>
2015-02-16 19:46:25 +0000

    premier push

---
+++ common.h
@@ -0,0 +1,19 @@
+
+
+struct aes_data_t{
+
+  char username[16];
+  unsigned int timestamp;
+  unsigned int ip;
+  unsigned short port;
+  char protocol;
+  char md5sum[32];
+
+};
+
+struct udp_data_t{
+
+  char header[3];
+  struct aes_data_t data;
+
+};
