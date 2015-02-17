5e6f47267697ea586b5207078a2fe50c905ba84c
tranxen <envobsolet@gmail.com>
2015-02-17 16:09:43 +0100

    on a viré le le header SPA, ntm

--- util.c
+++ util.c
@@ -2,6 +2,7 @@
 #include "util.h"
 #include <string.h>
 #include <stdio.h>
+#include <openssl/md5.h>
 
 void conv_ip_int_to_str(int ip, char* str){
 
@@ -13,3 +14,17 @@ void conv_ip_int_to_str(int ip, char* str){
 	  ip >> 16 & 0xFF,
 	  ip >> 24 & 0xFF);
 }
+
+void md5_hash_from_string (char *string, char *hash)
+{
+  int i;
+  char unsigned md5[MD5_DIGEST_LENGTH] = {0};
+
+  MD5((const unsigned char *)string, strlen(string), md5);
+
+  for (i=0; i < MD5_DIGEST_LENGTH; i++) {
+    sprintf(hash + 2*i, "%02x", md5[i]);
+  }
+}
+
+
