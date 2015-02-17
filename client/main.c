b23e307c324c4c18b20c097305ada959a3d9aeaf
guitoune <guillaume.desbieys@gmail.com>
2015-02-17 16:11:32 +0100

    Fill structure and send it

--- client/main.c
+++ client/main.c
@@ -1,14 +1,50 @@
 // include de common.h
+#include "../common.h"
 #include "udp_packet.h"
+#include <time.h>
+#include "../util.h"
+
+// struct aes_data_t{
+
+//   char username[16];
+//   unsigned int timestamp;
+//   unsigned int ip;
+//   unsigned short port;
+//   char protocol;
+//   char md5sum[32];
+
+// };
 
 int main() {
 
 	char ip_addr_str[] = "127.0.0.1";
 	char dest_port_str[] = "7777";
 
-	char payload[] = "SPACOOOLL";
+	//char payload[] = "SPACOOOLL";
 
-	send_udp_packet(ip_addr_str, dest_port_str, payload);
+	struct aes_data_t spa;
+
+	memset(spa.username, '\0', 16);
+	strcat(spa.username, "Superman");
+
+	spa.timestamp = (unsigned)time(NULL);
+
+	spa.ip = ip_to_int("127.0.0.1");
+	spa.port = 22;
+	spa.protocol = '0';
+
+	memset(spa.md5sum, '\0', 32);
+	strcat(spa.md5sum, "2eqkjhcizuedjksj");
+
+	int len = sizeof(struct aes_data_t);
+	char *buffer = NULL;
+	buffer = malloc(len);
+	memcpy(buffer, &spa, len);
+
+	send_udp_packet(ip_addr_str, dest_port_str, buffer);
+
+	free(buffer);
+
 
 	return 0;
 }
