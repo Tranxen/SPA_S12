d7f8e58fda76cb551fcc51e88caaaea9977ce739
guitoune <guillaume.desbieys@gmail.com>
2015-02-17 16:10:44 +0100

    Add ip_to_int

--- util.c
+++ util.c
@@ -13,3 +13,44 @@ void conv_ip_int_to_str(int ip, char* str){
 	  ip >> 16 & 0xFF,
 	  ip >> 24 & 0xFF);
 }
+
+unsigned int ip_to_int (const char * ip)
+{
+    /* The return value. */
+    unsigned v = 0;
+    /* The count of the number of bytes processed. */
+    int i;
+    /* A pointer to the next digit to process. */
+    const char * start;
+
+    start = ip;
+    for (i = 0; i < 4; i++) {
+        /* The digit being processed. */
+        char c;
+        /* The value of this byte. */
+        int n = 0;
+        while (1) {
+            c = * start;
+            start++;
+            if (c >= '0' && c <= '9') {
+                n *= 10;
+                n += c - '0';
+            }
+            /* We insist on stopping at "." if we are still parsing
+               the first, second, or third numbers. If we have reached
+               the end of the numbers, we will allow any character. */
+            else if ((i < 3 && c == '.') || i == 3) {
+                break;
+            }
+            else {
+                return 0;
+            }
+        }
+        if (n >= 256) {
+            return 0;
+        }
+        v *= 256;
+        v += n;
+    }
+    return v;
+}
--- util.h
+++ util.h
@@ -1,2 +1,3 @@
 
 void conv_ip_int_to_str(int ip, char* str);
+unsigned int ip_to_int (const char * ip);
