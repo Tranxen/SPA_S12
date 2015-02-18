73fbb9bb3aea58ac0b3bd8a6e70fcd6fe0ede6c2
guitoune <guillaume.desbieys@gmail.com>
2015-02-18 14:45:12 +0100

    Add option to choose interface

--- client/main.c
+++ client/main.c
@@ -31,14 +31,13 @@ int main(int argc, char *argv[]) {
 
 	srand(time(NULL));
 
-	if (argc != 6) {
-		printf("Usage : %s username ip ip_requested port_requested tcp/udp\n", argv[0]);
+	if (argc < 6) {
+		printf("Usage : %s username ip ip_requested port_requested tcp/udp [-i interface]\n", argv[0]);
 		printf("example : %s toto 127.0.0.1 192.168.1.1 22 tcp\n", argv[0]);
 		return 0;
 	}
 
-	char key[] = "fabien brillant";
-
+	
 	char *username = argv[1];
 	char *ip_addr_str = argv[2];
 	char *ip_requested = argv[3];
@@ -46,6 +45,27 @@ int main(int argc, char *argv[]) {
 	char *protocol_requested_str = argv[5];
 	lower(protocol_requested_str);
 	int protocol_requested;
+	char *interface = NULL;
+
+	int c;
+
+	while ((c = getopt (argc, argv, "i:")) != -1) {
+		switch (c) {
+			case 'i':
+				interface = optarg;
+				break;
+			case '?':
+				if (optopt == 'i')
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+				else if (isprint (optopt))
+          			fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        		else
+          			fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+		}
+	}
+
+	char key[] = "fabien brillant";
+
 
 	if (strcmp(protocol_requested_str, "tcp") == 0)
 		protocol_requested = TCP;
@@ -63,8 +83,8 @@ int main(int argc, char *argv[]) {
 
 	struct in_addr inp;
 
-	inet_aton("127.0.0.1", &inp);
-	spa.ip_src = (int)inp.s_addr; // Must be dynamic
+	inet_aton(get_ip_addr(interface), &inp);
+	spa.ip_src = (int)inp.s_addr;
 
   	inet_aton(ip_requested, &inp);
 	spa.ip_dst = (int)inp.s_addr;
@@ -83,7 +103,7 @@ int main(int argc, char *argv[]) {
 
 	char *cipher_text = encrypt(key, (char*)&spa, sizeof(struct aes_data_t));
 
-	send_udp_packet(ip_addr_str, dest_port, cipher_text);
+	send_udp_packet(interface, ip_addr_str, dest_port, cipher_text);
 
 	return 0;
 }
