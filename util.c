e158306c15b1cae822abc280ba4ee30cb2e65630
guitoune <guillaume.desbieys@gmail.com>
2015-02-18 10:40:30 +0100

    Remove commented fonction (md5)

--- util.c
+++ util.c
@@ -2,7 +2,6 @@
 #include "util.h"
 #include <string.h>
 #include <stdio.h>
-//#include <openssl/md5.h>
 
 void conv_ip_int_to_str(int ip, char* str){
 
@@ -14,18 +13,3 @@ void conv_ip_int_to_str(int ip, char* str){
 	  ip >> 16 & 0xFF,
 	  ip >> 24 & 0xFF);
 }
-
-/*
-void md5_hash_from_string (char *string, char *hash)
-{
-  int i;
-  char unsigned md5[MD5_DIGEST_LENGTH] = {0};
-
-  MD5((const unsigned char *)string, strlen(string), md5);
-
-  for (i=0; i < MD5_DIGEST_LENGTH; i++) {
-    sprintf(hash + 2*i, "%02x", md5[i]);
-  }
-}
-
-*/
--- util.h
+++ util.h
@@ -1,3 +1,2 @@
 
 void conv_ip_int_to_str(int ip, char* str);
-//void md5_hash_from_string (char *string, char *hash)
