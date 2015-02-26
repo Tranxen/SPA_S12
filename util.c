777286c806079b7c063d6334b265c56122289107
guitoune <guillaume.desbieys@gmail.com>
2015-02-26 16:02:43 +0100

    Added a random string generator

--- util.c
+++ util.c
@@ -23,4 +23,17 @@ inline void conv_ip_str_to_int(int* ip, char* str){
   inet_aton(str, &inp);
   *ip = (int)inp.s_addr;
 
-};
+}
+
+inline void rand_string(char *str, size_t size) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (size) {
+        --size;
+        size_t n;
+        for (n = 0; n < size; n++) {
+            int key = rand() % (int) (sizeof charset - 1);
+            str[n] = charset[key];
+        }
+        str[size] = '\0';
+    }
+}
--- util.h
+++ util.h
@@ -1,4 +1,12 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdlib.h>
 
 inline void conv_ip_int_to_str(int ip, char* str);
 
 inline void conv_ip_str_to_int(int* ip, char* str);
+
+inline void rand_string(char *str, size_t size);
+
+#endif UTIL_H
