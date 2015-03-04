fd01869536a4560bf862bb152fd51abc987ca2ee
guitoun3 <guitoun3@users.noreply.github.com>
2015-03-04 16:01:08 +0100

    Fucking weird behavior

--- md5.c
+++ md5.c
@@ -5,7 +5,7 @@ void md5_hash_from_string (char *string, int size, char *hash)
   int i;
   char unsigned md5[MD5_DIGEST_LENGTH] = {0};
 
-  MD5((const unsigned char *)string, size, md5);
+  MD5((const unsigned char *)string, size-1, md5);
 
   for (i=0; i < MD5_DIGEST_LENGTH; i++) {
     sprintf(hash + 2*i, "%02x", md5[i]);
